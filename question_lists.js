ab7d2b4ae7430ee3a9d1bee622ee193536ea733f
amalsantosa <120997152+IchlashulAmalSantosa@users.noreply.github.com>
2024-11-05 05:11:29 +0700

    Add files via upload

---
+++ question_lists.js
@@ -0,0 +1,30 @@
+var quiz = [
+  [1, 1, "Tempat wisata favorit di Bandung", "LEMBANG", "datar"],
+  [1, 3, "Mie khas Bandung yang menggunakan toping kikil", "MIEKOCOK", "turun"],
+  [
+    1,
+    5,
+    "Daerah di Bandung yang terkenal dengan lukisan muralnya",
+    "ANTAPANI",
+    "turun",
+  ],
+  [5, 7, "Tempat jual beli elektronik populer di Bandung", "BEC", "datar"],
+  [6, 5, "Tempat rekreasi keluarga di Bandung timur", "ALUNALUNUBER", "datar"],
+  [1, 7, "Tempat jogging paling terkenal di Bandung", "GASIBU", "turun"],
+  [5, 9, "Pusat grosir sayur di Bandung", "CARINGIN", "turun"],
+  [10, 9, "Markas Persib Bandung", "GBLA", "datar"],
+  [
+    12,
+    7,
+    "Campuran sayuran dan kelapa parut dengan bumbu khas Sunda",
+    "URAB",
+    "turun",
+  ],
+  [
+    12,
+    6,
+    "Hidangan berkuah yang cocok dinikmati saat cuaca dingin",
+    "CUANKI",
+    "datar",
+  ],
+];
